5a7749650d53ab4274353bf6a12e73669698a4a0
Devon Reihl <devon.reihl@gmail.com>
2020-09-15 12:05:28 -0700

    Inital commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  res.send('Hello, world!');
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
